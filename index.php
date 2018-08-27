4a207c0896bc70e3a26faf522ac25742b40fe926
dembasy <dembasy1227@gmail.com>
2018-08-27 10:31:39 +0200

    exercice 4 fini

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html> 
+<html>
+    <head>
+        <title>ex4</title>
+    </head>
+    <body>
+        <p>
+            <?= 
+            // Affichage du timestamp à la date actuel 
+            'Timestamp actuel :'. time();
+            ?> 
+        </p>
+    </body>
+</html>
