aabd6b6fd96df799f27287f407c14175b1cac9db
dembasy <dembasy1227@gmail.com>
2018-08-27 10:40:16 +0200

    ajout du tsp du mardi 2 août

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
+<?php 
+    //definition du timestamp qui sera utilisé pour le mardi 2 août
+    $tsp = mktime(15, 0, 0, 2, 8, 2015);
+?>
+
+
 <!DOCTYPE html> 
 <html>
     <head>
         <title>ex4</title>
     </head>
     <body>
-        <p>
-            <?= 
-            // Affichage du timestamp à la date actuel 
-            'Timestamp actuel :'. time();
-            ?> 
-        </p>
+        <p> <?= 'Timestamp actuel :'. time(); ?> </p>
+        <p> <?= 'Timestamp du mardi 2 août :'. $tsp; ?> </p>
     </body>
 </html>
